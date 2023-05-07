d3d8efee3704901288d663a8daf1ace29cc07704
Marian25 <marian.stanciulica@gmail.com>
2023-05-07 16:16:03 +0300

    Remove confirmPassword from ChangePasswordRequest

--- Sources/App/Models/DTO/Authentication/Change Password/ChangePasswordRequest.swift
+++ Sources/App/Models/DTO/Authentication/Change Password/ChangePasswordRequest.swift
@@ -3,13 +3,11 @@ import Vapor
 struct ChangePasswordRequest: Content {
     let currentPassword: String
     let newPassword: String
-    let confirmPassword: String
 }
 
 extension ChangePasswordRequest: Validatable {
     static func validations(_ validations: inout Validations) {
         validations.add("currentPassword", as: String.self, is: .count(8...))
         validations.add("newPassword", as: String.self, is: .count(8...))
-        validations.add("confirmPassword", as: String.self, is: .count(8...))
     }
 }
